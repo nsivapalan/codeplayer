f16498cc651c189734cd27ed1dfa67c9b6d1a90a
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2015-01-20 13:22:09 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var windowHeight = $(window).height();
+var topBarHeight = $("#topBar").height();
+var codeContainerHeight = windowHeight-topBarHeight;
+
+$(".codeContainer").height(codeContainerHeight +"px");
+
+$(".toggle").click(function(){
+
+	$(this).toggleClass("selected");
+
+	var activeDiv = $(this).html();
+
+	$("#"+activeDiv+"Container").toggle();
+
+	var showingDivs=$(".codeContainer").filter(function() {
+
+		return($(this).css("display")!="none");
+
+	}).length;
+
+	var width = 100/showingDivs;
+
+	$(".codeContainer").width(width+"%");
+
+
+});
+
+$("#runButton").click(function() {
+
+	$("iframe").contents().find("html").html('<style>'+$("#cssCode").val()+'</style>'+$("#htmlCode").val());
+
+	document.getElementById("resultFrame").contentWindow.eval($("#jsCode").val());
+
+});
